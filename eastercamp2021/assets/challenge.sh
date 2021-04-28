eff0bd73a7789b53faf7cc87908d49875011d62c
Norwin Schnyder <norwin.schnyder+github@gmail.com>
2021-04-28 09:50:18 +0200

    Fix challenge.sh

--- eastercamp2021/assets/challenge.sh
+++ eastercamp2021/assets/challenge.sh
@@ -6,12 +6,12 @@ function kc_start_tasks()
       "Herzlich willkommen! Du bist nun ein Teil der Crew.\n" \
 
   kc_task \
-      "$(tput sgr 0)$(tput setaf 4) Um herauszufinden in welchem Raum du dich gerade befindest Tippe $(tput setaf 3)pwd$(tput sgr 0)$(tput setaf 4) in das Terminal (links) und bestätige mit der <Enter> Taste.$(tput sgr 0)\n\n" \
-      "\U0001f389 Gut gemacht!\n\nDu siehst du befindest dich gerade in der $(tput setaf 1)$(tput setab 7)Cafeteria$(tput sgr 0). Das Kommando $(tput setaf 3)pwd$(tput sgr 0) (Abkürzung für Englisch 'print working directory' 'zeige aktuellen Ordner') zeigt dir immer an wo Du dich gerade befindest. Dabei wir dir der ganze Pfad angezeigt und das letzte Element ist jeweils das Ordner and dem du dich befindest.\n\n\nNotiere dir unbedingt jedes Kommando und was du damit machen kannst, du weisst ja nicht wann du es das nächste mal wieder benötigst.\n" \
+      "$(tput sgr 0)$(tput setaf 6) Um herauszufinden in welchem Raum du dich gerade befindest Tippe $(tput setaf 3)pwd$(tput sgr 0)$(tput setaf 6) in das Terminal (links) und bestätige mit der <Enter> Taste.$(tput sgr 0)\n\n" \
+      "\U0001f389  $(tput setaf 2)Gut gemacht!$(tput sgr 0)\n\nDu siehst du befindest dich gerade in der $(tput setaf 1)$(tput setab 7)Cafeteria$(tput sgr 0). Das Kommando $(tput setaf 3)pwd$(tput sgr 0) (Abkürzung für Englisch 'print working directory' 'zeige aktuellen Ordner') zeigt dir immer an wo Du dich gerade befindest. Dabei wir dir der ganze Pfad angezeigt und das letzte Element ist jeweils das Ordner and dem du dich befindest.\n\n\nNotiere dir unbedingt jedes Kommando und was du damit machen kannst, du weisst ja nicht wann du es das nächste mal wieder benötigst.\n" \
       "cat /tmp/pwd | grep 1"
 
   kc_task \
-      "$(tput sgr 0)$(tput setaf 4) Du kannst dich in einem Raum (Ordner) umzusehen in dem du dich gerade befindest mit dem Kommando $(tput setaf 3)ls$(tput sgr 0)$(tput setaf 4). Probiere es doch gleich mal aus.$(tput sgr 0)" \
-      "\nEs gibt hier in der Cafeteria verschiedene Gegenstände (Dateien) mit denen du interagieren kannst und weitere Rauem (Ordner)  welche du betreten kannst.\n" \
+      "$(tput sgr 0)$(tput setaf 6) Du kannst dich in einem Raum umzusehen in dem du dich gerade befindest mit dem Kommando $(tput setaf 3)ls$(tput sgr 0)$(tput setaf 6). Probiere es doch gleich mal aus.$(tput sgr 0)" \
+      "\u2728 $(tput setaf 2)Super!$(tput sgr 0)\n\nEs gibt hier in der Cafeteria verschiedene Gegenstände (Dateien) mit denen du interagieren kannst und weitere Rauem (Ordner)  welche du betreten kannst.\n" \
       "cat /tmp/ls | grep 1"
 }
