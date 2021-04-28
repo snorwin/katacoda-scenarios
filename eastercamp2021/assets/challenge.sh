198a6589b6cb4c78f6a07281db6458cf41544b9d
Norwin Schnyder <norwin.schnyder+github@gmail.com>
2021-04-28 13:02:08 +0200

    Fix challenge.sh

--- eastercamp2021/assets/challenge.sh
+++ eastercamp2021/assets/challenge.sh
@@ -10,7 +10,7 @@ function kc_start_tasks()
   export FMT_OPTIONS=""
 
   kc_task \
-      "$(tput sgr 0)$(tput setaf 6) Um herauszufinden in welchem Raum du dich gerade befindest Tippe $(tput setaf 3)pwd$(tput sgr 0)$(tput setaf 6) in das Terminal (links) und bestätige mit der <Enter> Taste.$(tput sgr 0)\n\n" \
+      "$(tput sgr 0)$(tput setaf 6) Um herauszufinden in welchem Raum du dich gerade befindest Tippe $(tput setaf 3)pwd$(tput sgr 0)$(tput setaf 6) in das Terminal (links) und bestätige jedes Kommando mit der $(tput setaf 3)ENTER$(tput sgr 0)$(tput setaf 6) Taste.$(tput sgr 0)\n\n" \
       "\U0001f389  $(tput setaf 2)Gut gemacht!$(tput sgr 0)\n\nDu siehst du befindest dich gerade in der $(tput setaf 1)$(tput setab 7)Cafeteria$(tput sgr 0). Das Kommando $(tput setaf 3)pwd$(tput sgr 0) (Abkürzung für Englisch 'print working directory' 'zeige aktuellen Ordner') zeigt dir immer an wo Du dich gerade befindest. Dabei wir dir der ganze Pfad angezeigt und das letzte Element ist jeweils das Ordner and dem du dich befindest.\n\n\nNotiere dir unbedingt jedes Kommando und was du damit machen kannst, du weisst ja nicht wann du es das nächste mal wieder benötigst.\n" \
       "cat /tmp/pwd | grep 1"
 
@@ -21,6 +21,11 @@ function kc_start_tasks()
 
   kc_task \
       "$(tput sgr 0)$(tput setaf 6) Damit du einen Raum betreten kannst musst du das Kommando $(tput setaf 3)cd$(tput sgr 0)$(tput setaf 6) gefolgt von dem Namen des Ordner verwenden. Tippe $(tput setaf 3)cd Admin$(tput sgr 0)$(tput setaf 6) ins Terminal um in den $(tput setaf 1)$(tput setab 7)Admin$(tput sgr 0)$(tput setaf 6)  zu gelangen.\n\n" \
-      "\U0001f4ab  $(tput setaf 2)Perfekt!$(tput sgr 0)\n" \
+      "\U0001f4ab  $(tput setaf 2)Perfekt!$(tput sgr 0)\n\nFalls du jetzt wieder in den Raum von vorher zurück mochtest kannst du einfach $(tput setaf 3)cd ..$(tput sgr 0)$(tput setaf 6) eingeben." \
       "pgrep bash | xargs -L1 pwdx | grep Admin"
+
+  kc_task \
+    "$(tput sgr 0)$(tput setaf 6) Um das Spiel zu starten, gehe nun zurück in die $(tput setaf 1)$(tput setab 7)Cafeteria$(tput sgr 0).\n\n" \
+    "\nFalls du dich mal komplett verirrt hast, verwende das Kommando $(tput setaf 3)cd /AmongUs/Cafeteria$(tput sgr 0)$(tput setaf 6) um immer wieder in die $(tput setaf 1)$(tput setab 7)Cafeteria$(tput sgr 0) zu gelangen." \
+    "! pgrep bash | xargs -L1 pwdx | grep -v Cafeteria/"
 }
