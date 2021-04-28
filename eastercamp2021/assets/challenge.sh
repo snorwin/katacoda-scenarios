aae3e1721595b1291d70d64a14b2f3fb2da4fe00
Norwin Schnyder <norwin.schnyder+github@gmail.com>
2021-04-28 08:47:57 +0200

    Fix challenge.sh

--- eastercamp2021/assets/challenge.sh
+++ eastercamp2021/assets/challenge.sh
@@ -5,8 +5,6 @@ function kc_start_tasks()
   kc_instructions \
       "Herzlich willkommen! Du bist nun ein Teil der Crew.\n" \
 
-  alias pwd='pwd && echo 1 > /tmp/pwd'
-
   kc_task \
       "Um herauszufinden in welchem Raum du dich gerade befindest Tippe \`pwd\` in das Terminal (links) und bestätige mit <Enter> Taste." \
       "Gut gemacht! Du siehst du befindest dich gerade in der Cafeteria. Das Kommando \`pwd\` (Abkürzung für Englisch 'print working directory' 'zeige aktuellen Ordner') zeigt dir immer an wo Du dich gerade befindest. Dabei wir dir der ganze Pfad angezeigt und das letzte Element ist jeweils das Ordner and dem du dich befindest." \
--- eastercamp2021/configure.sh
+++ eastercamp2021/configure.sh
@@ -3,6 +3,10 @@ launch.sh
 apt install bc
 
 mkdir /AmongUs/
+mkdir /AmongUs/Cafeteria
 
+cd /AmongUs/Cafeteria
+
+alias pwd='pwd && echo 1 > /tmp/pwd'
 
 echo 'done' > /opt/katacoda-background-finished
