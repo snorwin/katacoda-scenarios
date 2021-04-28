22cc0fd42517de71ba04c11cc0bbc00623a4c53d
Norwin Schnyder <norwin.schnyder+github@gmail.com>
2021-04-28 09:27:39 +0200

    Fix challenge.sh

--- eastercamp2021/assets/challenge.sh
+++ eastercamp2021/assets/challenge.sh
@@ -6,12 +6,12 @@ function kc_start_tasks()
       "Herzlich willkommen! Du bist nun ein Teil der Crew.\n" \
 
   kc_task \
-      "$(tput sgr 0) Um herauszufinden in welchem Raum du dich gerade befindest Tippe $(tput setaf 3)pwd$(tput sgr 0) in das Terminal (links) und bestätige mit <Enter> Taste.\n\n" \
+      "$(tput sgr 0) Um herauszufinden in welchem Raum du dich gerade befindest Tippe $(tput setaf 3)pwd$(tput sgr 0) in das Terminal (links) und bestätige mit der <Enter> Taste.\n\n" \
       "Gut gemacht!\n\nDu siehst du befindest dich gerade in der $(tput setaf 1)$(tput setab 7)Cafeteria$(tput sgr 0). Das Kommando $(tput setaf 3)pwd$(tput sgr 0) (Abkürzung für Englisch 'print working directory' 'zeige aktuellen Ordner') zeigt dir immer an wo Du dich gerade befindest. Dabei wir dir der ganze Pfad angezeigt und das letzte Element ist jeweils das Ordner and dem du dich befindest.\n\n\nNotiere dir unbedingt jedes Kommando und was du damit machen kannst, du weisst ja nicht wann du es das nächste mal wieder benötigst.\n" \
       "cat /tmp/pwd | grep 1"
 
   kc_task \
       "$(tput sgr 0) Du kannst dich in einem Raum (Ordner) umzusehen in dem du dich gerade befindest mit dem Kommando $(tput setaf 3)ls$(tput sgr 0). Probiere es doch gleich mal aus." \
-      "Es gibt hier in der Cafeteria verschiedene Gegenstände (Dateien) mit denen du interagieren kannst und weitere Rauem (Ordner)  welch du betreten kannst.\n" \
+      "\nEs gibt hier in der Cafeteria verschiedene Gegenstände (Dateien) mit denen du interagieren kannst und weitere Rauem (Ordner)  welch du betreten kannst.\n" \
       "cat /tmp/ls | grep 1"
 }
--- eastercamp2021/configure.sh
+++ eastercamp2021/configure.sh
@@ -7,9 +7,9 @@ sed -i 's/echo "Complete the following tasks to progress:"//' /usr/local/bin/cha
 mkdir /AmongUs/
 mkdir /AmongUs/Cafeteria
 
-cd /AmongUs/Cafeteria
-
 echo "alias pwd='pwd && echo 1 > /tmp/pwd'" >> /root/.bashrc
 echo "alias ls='ls && echo 1 > /tmp/ls'" >> /root/.bashrc
+
+echo "cd /AmongUs/Cafeteria'" >> /root/.bashrc
 
 echo 'done' > /opt/katacoda-background-finished
