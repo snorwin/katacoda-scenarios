84eebc4a2d793ceba431f4b81bc60ed88dd53b77
Norwin Schnyder <norwin.schnyder+github@gmail.com>
2021-04-28 11:43:21 +0200

    Fix challenge.sh

--- eastercamp2021/configure.sh
+++ eastercamp2021/configure.sh
@@ -3,6 +3,9 @@ launch.sh
 apt install bc
 
 sed -i 's/echo "Complete the following tasks to progress:"//' /usr/local/bin/challenge.sh
+sed -i 's/printf "\x1b[32mKatacoda Challenge\x1b[0m\n";//' /usr/local/bin/challenge.sh
+sed -i 's/printf "Please share your feedback:\n";//' /usr/local/bin/challenge.sh
+sed -i 's/printf "https://get.oreilly.com/skills-challenge-survey.html\n";//' /usr/local/bin/challenge.sh
 
 mkdir -p /AmongUs/
 mkdir -p /AmongUs/Cafeteria
