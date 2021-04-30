bd003d4511fb99bd307017cb67286ade0db778d8
Norwin Schnyder <norwin.schnyder+github@gmail.com>
2021-04-30 12:36:06 +0200

    Fix challenge.sh

--- eastercamp2021/configure.sh
+++ eastercamp2021/configure.sh
@@ -4,7 +4,7 @@ apt install bc
 
 sed -i 's/echo "Complete the following tasks to progress:"//' /usr/local/bin/challenge.sh
 sed -i 's/Completed Task/Aufgabe erledigt/' /usr/local/bin/challenge.sh
-sed -i 's/Congratulations. You mastered the challenge in \$taken!/\\U0001f389  Herzlichen Glückwunsch, deine Crew und du haben das Raumschiff in \$taken gerettet!/' /usr/local/bin/challenge.sh
+sed -i 's/Congratulations. You mastered the challenge in \$taken!/\U0001f389  Herzlichen Glückwunsch, deine Crew und du haben das Raumschiff in \$taken gerettet!/' /usr/local/bin/challenge.sh
 sed -i 's/echo "Completed Challenge"//' /usr/local/bin/challenge.sh
 sed -i 's/Katacoda Challenge//' /usr/local/bin/challenge.sh
 sed -i 's/Please share your feedback://' /usr/local/bin/challenge.sh
